823c56c7f94a20e45aa98e0f2053c5c316ee0d8d
Aditya Pradita <pradita397@gmail.com>
2019-12-02 18:24:22 +0700

    Initiate project Vue

---
+++ k-festival/src/App.vue
@@ -0,0 +1,18 @@
+<script>
+import Header from './components/Header.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Header
+  }
+}
+</script>
+
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <Header />
+  </div>
+</template>
---
+++ k-festival/src/components/Header.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="k-festival-wrap">
+        <div class="header-img-container">
+            <img class="background-body visible-xs" src="https://ecm-prd-pub.s3.ap-southeast-1.amazonaws.com/kfestival/assets/img/header-bg.png" width="100%"/>
+            <img class="background-body hidden-xs" src="https://ecm-prd-pub.s3.ap-southeast-1.amazonaws.com/kfestival/assets/img/background.jpg" width="100%"/>
+            <div class="container-fluid">
+                <ul class="nav nav-tabs main-tabs" role="tablist">
+                    <li role="presentation" class="event-anchor-list">
+                        <a href="#events" aria-controls="event" class="event-anchor" >Highlight</a>
+                    </li>
+                    <li role="presentation" class="korea-ticket-anchor-list">
+                        <a href="#liburan-gratis-ke-korea" title="Liburan Gratis Ke Korea" class="korea-ticket-anchor">Liburan Gratis ke Korea</a>
+                    </li>
+                    <!-- <li role="presentation" class="k-pop-festival-anchor-list">
+                        <a class="k-pop-festival-anchor" href="#k-pop-concert">K-Pop Festival</a>
+                    </li> -->
+                    <li role="presentation" class="performance-ticket-anchor-list">
+                        <a class="performance-ticket-anchor" href="#tiket-pertunjukkan-gratis" title="Tiket Pertunjukkan Gratis">Tiket Pertunjukkan Gratis</a>
+                    </li>
+                    <li role="presentation" class="k-sale-festa-anchor-list">
+                        <a href="https://www.ilotte.com/promo-korean-festival-000000131515.do?ch=ETCTS5WKIV" class="k-sale-festa-anchor">Korean Bazaar</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+    name: 'Header'
+}
+</script>
