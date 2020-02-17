153f9c8d8199b6e9c6b985290573f3778494ea27
Aditya Pradita <pradita397@gmail.com>
2020-02-17 16:57:00 +0700

    Add vue config js

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,8 @@
 module.exports = {
     chainWebpack: config => {
         config.module.rules.delete('eslint');
-    }
+    },
+    publicPath: process.env.NODE_ENV === 'production'
+    ? '/K-Festival-Page-Vue/'
+    : '/'
 }
