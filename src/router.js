9883f863a858d244029a0f19beb2bdcc53a87537
Aditya Pradita <pradita397@gmail.com>
2020-02-19 13:38:20 +0700

    Configure for netlify

--- src/router.js
+++ src/router.js
@@ -9,6 +9,7 @@ Vue.use(Router)
 
 export default new Router({
     base: __dirname,
+    mode: 'history',
     linkExactActiveClass: 'active',
     routes: [
         { 
--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,5 @@
 module.exports = {
     chainWebpack: config => {
         config.module.rules.delete('eslint');
-    },
-    publicPath: process.env.NODE_ENV === 'production'
-    ? '/K-Festival-Page-Vue/'
-    : '/'
+    }
 }
